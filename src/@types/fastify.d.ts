6eee0ee1019887060f52e2fbf971a07492133d62
Anderson Foschiera <foschiera@Mac-mini-de-Anderson.local>
2026-05-25 04:59:36 -0300

    feat: Add types to send mail

--- src/@types/fastify.d.ts
+++ src/@types/fastify.d.ts
@@ -3,6 +3,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 declare module 'fastify' {
   interface FastifyInstance {
     authenticate(request: FastifyRequest, reply: FastifyReply): Promise<void>
+    sendEmail(to: string, subject: string, html: string): Promise<void>
   }
 }
 
